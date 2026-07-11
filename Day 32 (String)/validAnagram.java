7645c4de5fe13ae4942f931244177c011b546964
Abhay Gupta <abhaygupta9397@gmail.com>
2026-07-12 00:33:22 +0530

    Day 32 done

---
+++ Day 32 (String)/validAnagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int fre[] = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            fre[s.charAt(i) - 'a']++;
+            fre[t.charAt(i) - 'a']--;
+        }
+
+        for(int f : fre){
+            if(f != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
